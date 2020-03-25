299a01555a2ef9e3a7514500f2d5200f53ee4e6f
hamstar0 <webutek@gmail.com>
2020-03-24 17:26:45 -0700

    v0.9.6 - Fixed speedloader loaded texture (untested)

--- TheMadRanger/Items/SpeedloaderItem.cs
+++ TheMadRanger/Items/SpeedloaderItem.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
@@ -9,7 +11,7 @@ using TheMadRanger.Helpers.Misc;
 
 
 namespace TheMadRanger.Items {
-	class SpeedloaderItem : ModItem {
+	partial class SpeedloaderItem : ModItem {
 		public static int Width = 10;
 		public static int Height = 10;
 
@@ -27,7 +29,7 @@ namespace TheMadRanger.Items {
 		public override string Texture {
 			get {
 				if( this.LoadedRounds > 0 ) {
-					return "Items/SpeedloaderItem_Loaded";
+					return "TheMadRanger/Items/SpeedloaderItem_Loaded";
 				}
 				return base.Texture;
 			}
@@ -79,6 +81,79 @@ namespace TheMadRanger.Items {
 			};
 
 			return tag;
+		}
+
+
+		////////////////
+		
+		public override bool PreDrawInInventory(
+					SpriteBatch spriteBatch,
+					Vector2 position,
+					Rectangle frame,
+					Color drawColor,
+					Color itemColor,
+					Vector2 origin,
+					float scale ) {
+			Texture2D tex = Main.itemTexture[ this.item.type ];
+			if( this.LoadedRounds > 0 ) {
+				tex = ModContent.GetTexture( this.Texture );
+			}
+
+			spriteBatch.Draw( tex, position, frame, drawColor, 0f, origin, scale, SpriteEffects.None, 0f );
+			if( this.item.color != Color.Transparent ) {
+				spriteBatch.Draw( tex, position, frame, itemColor, 0f, origin, scale, SpriteEffects.None, 0f );
+			}
+
+			return false;
+		}
+		
+		public override void PostDrawInWorld(
+					SpriteBatch spriteBatch,
+					Color lightColor,
+					Color alphaColor,
+					float rotation,
+					float scale,
+					int whoAmI ) {
+			Texture2D tex = Main.itemTexture[this.item.type];
+			if( this.LoadedRounds > 0 ) {
+				tex = ModContent.GetTexture( this.Texture );
+			}
+
+			float offsetX = this.item.height - tex.Height;
+			float offsetY = (this.item.width / 2) - (tex.Width / 2);
+
+			Color alpha = this.item.GetAlpha( lightColor );
+
+			Main.spriteBatch.Draw(
+				tex,
+				(this.item.position - Main.screenPosition) + new Vector2(
+					(float)( tex.Width / 2 ) + offsetY,
+					(float)( tex.Height / 2 ) + offsetX + 2f
+				),
+				new Rectangle( 0, 0, tex.Width, tex.Height ),
+				alpha,
+				rotation,
+				new Vector2( tex.Width / 2, tex.Height / 2 ),
+				scale,
+				SpriteEffects.None,
+				0f
+			);
+			if( this.item.color != default(Color) ) {
+				Main.spriteBatch.Draw(
+					tex,
+					(this.item.position - Main.screenPosition) + new Vector2(
+						(float)( tex.Width / 2 ) + offsetY,
+						(float)( tex.Height / 2 ) + offsetX + 2f
+					),
+					new Rectangle( 0, 0, tex.Width, tex.Height ),
+					this.item.GetColor( lightColor ),
+					rotation,
+					new Vector2( tex.Width / 2, tex.Height / 2 ),
+					scale,
+					SpriteEffects.None,
+					0f
+				);
+			}
 		}
 
 
