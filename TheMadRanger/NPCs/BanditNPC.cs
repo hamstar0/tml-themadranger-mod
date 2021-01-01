070cba208825c6d5460d82f47823aee79d21dfba
hamstar0 <webutek@gmail.com>
2021-01-01 05:55:51 -0800

    v1.4.2 - Added bandit retreat behavior

--- TheMadRanger/NPCs/BanditNPC.cs
+++ TheMadRanger/NPCs/BanditNPC.cs
@@ -3,16 +3,56 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using HamstarHelpers.Helpers.Debug;
 
 
 namespace TheMadRanger.NPCs {
 	partial class BanditNPC : ModNPC {
-		public static bool IsFiring { get; private set; } = false;
+		public static int RetreatTickDuration { get; } = 60 * 15;
+		public static int ContactDamage { get; } = 10;
+		public static int ShotDamage { get; } = 10;
+		public static int RetreatTileDistance { get; } = 9;
+		public static float MaxChaseSpeed { get; } = 3.5f;
+		public static float MaxRetreatSpeed { get; } = 4f;
 
 		////
 
-		public static int ContactDamage { get; } = 10;
-		public static int ShotDamage { get; } = 20;
+		public static bool IsFiring { get; private set; } = false;
+
+
+
+		////////////////
+
+		public bool IsRetreatingNow {
+			get => this.RetreatTimer > 0;
+			private set {
+				if( value ) {
+					this.RetreatTimer = BanditNPC.RetreatTickDuration;
+				} else {
+					this.RetreatTimer = 0;
+				}
+			}
+		}
+
+		public bool IsBraveNow {
+			get => this.BraveryTimer > 0;
+			private set {
+				if( value ) {
+					this.BraveryTimer = 60 * 3;
+				} else {
+					this.BraveryTimer = 0;
+				}
+			}
+		}
+
+
+		////////////////
+
+		private int RetreatTimer = 0;
+
+		private int RetreatStuckElapsed = 0;
+
+		private int BraveryTimer = 0;
 
 
 
@@ -27,30 +67,34 @@ namespace TheMadRanger.NPCs {
 			NPC npc = this.npc;
 
 			// Adjust coin drop amount on a curve
-			float valueScale = Main.rand.NextFloat();
-			valueScale = valueScale * valueScale * valueScale * valueScale * valueScale * valueScale;
+			float valMul = Main.rand.NextFloat();
+			valMul = valMul * valMul * valMul * valMul * valMul * valMul;
 			int minVal = Item.buyPrice( 0, 0, 10, 0 );
 			int maxVal = Item.buyPrice( 0, 10, 0, 0 );
-			int value = minVal + (int)((float)(maxVal - minVal) * valueScale);
+			int value = minVal + (int)((float)(maxVal - minVal) * valMul);
 
 			npc.width = 18;
 			npc.height = 40;
-			npc.aiStyle = 3;
-			npc.damage = BanditNPC.ContactDamage;
-			npc.defense = 1;
-			npc.lifeMax = 50;
-			npc.HitSound = SoundID.NPCHit1;
-			npc.DeathSound = SoundID.NPCDeath1;
-			npc.knockBackResist = 0.3f;
-			npc.value = value;
-			npc.buffImmune[ BuffID.Poisoned ] = true;
-			npc.buffImmune[ BuffID.Confused ] = false;
 
+			npc.lifeMax = 50;
+			npc.defense = 1;
+			npc.damage = BanditNPC.ContactDamage;
+			npc.knockBackResist = 0.3f;
+
+			npc.value = value;
+
+			npc.aiStyle = 3;
 			this.aiType = NPCID.SkeletonSniper;
 			this.animationType = NPCID.SkeletonSniper;
 
 			//this.banner = Item.NPCtoBanner( NPCID.Zombie );
 			//this.bannerItem = Item.BannerToItem( this.banner );
+
+			npc.buffImmune[ BuffID.Poisoned ] = true;
+			npc.buffImmune[ BuffID.Confused ] = false;
+
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath1;
 		}
 
 
@@ -58,11 +102,39 @@ namespace TheMadRanger.NPCs {
 
 		public override bool PreAI() {
 			BanditNPC.IsFiring = true;
-			return true;
+
+			bool isRetreat = this.PreAI_ApplyRetreatEffectsIf();
+
+			this.PreAI_ApplyRetreatEffectsIf();
+
+			return isRetreat;
+		}
+
+		public override void AI() {
+//DebugHelpers.Print( "ai", string.Join(", ", this.npc.ai) );
+			this.AI_CheckRetreatIf();
 		}
 
 		public override void PostAI() {
 			BanditNPC.IsFiring = false;
+
+			this.PostAI_ApplyNormalMovementChangesIf();
+			this.PostAI_ApplyRetreatMovementChangesIf();
+		}
+
+
+		////////////////
+
+		public override void OnHitByItem( Player player, Item item, int damage, float knockback, bool crit ) {
+			if( !this.IsRetreatingNow && !this.IsBraveNow ) {
+				this.IsRetreatingNow = true;
+			}
+		}
+
+		public override void OnHitByProjectile( Projectile projectile, int damage, float knockback, bool crit ) {
+			if( !this.IsRetreatingNow && !this.IsBraveNow ) {
+				this.IsRetreatingNow = true;
+			}
 		}
 
 
---
+++ TheMadRanger/NPCs/BanditNPC_AI.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using HamstarHelpers.Helpers.Debug;
+
+
+namespace TheMadRanger.NPCs {
+	partial class BanditNPC : ModNPC {
+		private bool PreAI_ApplyRetreatEffectsIf() {
+			if( !this.IsRetreatingNow ) {
+				return true;
+			}
+
+			this.RetreatTimer--;
+
+			// Done retreating; don't relapse
+			if( this.RetreatTimer <= 0 ) {
+				this.IsRetreatingNow = false;	// redundant
+
+				this.IsBraveNow = true;
+
+				return true;
+			}
+
+			this.PreAI_ApplyRetreatIf_ApplyDirectionAndAI();
+			this.PreAI_ApplyRetreatIf_ApplyRun();
+
+			return false;
+		}
+
+		private void PreAI_ApplyRetreatIf_ApplyDirectionAndAI() {
+			this.npc.ai[0] = 1f;
+			this.npc.ai[1] = 0f;
+			this.npc.ai[2] = 0f;
+			this.npc.ai[3] = 0f;
+
+			Player player = Main.player[ this.npc.target ];
+
+			float dir = this.npc.oldDirection;
+			if( player?.active == true ) {
+				dir = (this.npc.Center.X - player.Center.X) < 0f
+						? -1f : 1f;
+			}
+
+			this.npc.direction = (int)dir;
+		}
+
+		private void PreAI_ApplyRetreatIf_ApplyRun() {
+			//bool isAimedInVelocityDir = (this.npc.direction > 1 && this.npc.velocity.X > 0f)
+			//	|| (this.npc.direction < 1 && this.npc.velocity.X < 0f);
+			if( Math.Abs(this.npc.velocity.X) < BanditNPC.MaxRetreatSpeed ) {
+				this.npc.velocity.X += (float)this.npc.direction * 0.1f;
+			}
+		}
+
+
+		////
+
+		private void AI_CheckRetreatIf() {
+			if( !this.npc.HasPlayerTarget || this.npc.target < 0 ) {
+				return;
+			}
+
+			Player player = Main.player[ this.npc.target ];
+			if( player?.active != true || player.dead ) {
+				return;
+			}
+
+			this.BraveryTimer--;
+
+			if( !this.IsBraveNow ) {
+				this.AI_CheckRetreatIf_Unbrave( player );
+			}
+		}
+
+		private void AI_CheckRetreatIf_Unbrave( Player targetPlayer ) {
+			int retreatDist = BanditNPC.RetreatTileDistance * 16;
+			int retreatDistSqr = retreatDist * retreatDist;
+
+			if( (targetPlayer.Center - npc.Center).LengthSquared() < retreatDistSqr ) {
+				this.IsRetreatingNow = true;
+			}
+		}
+
+
+		////
+
+		private void PostAI_ApplyNormalMovementChangesIf() {
+			if( this.IsRetreatingNow ) {
+				return;
+			}
+			if( this.npc.velocity.X == 0 ) {
+				return;
+			}
+			if( this.npc.ai[2] != 0f ) {
+				return;
+			}
+
+			if( Math.Abs(this.npc.velocity.X) < BanditNPC.MaxChaseSpeed ) {
+				this.npc.velocity.X += (float)this.npc.direction * 0.3f;
+			}
+		}
+
+		private void PostAI_ApplyRetreatMovementChangesIf() {
+			if( !this.IsRetreatingNow ) {
+				return;
+			}
+
+			if( this.npc.position.X == 0 && this.npc.velocity.Y == 0f ) {
+				this.RetreatStuckElapsed++;
+
+				this.npc.velocity.Y -= 8f;
+			} else {
+				this.RetreatStuckElapsed = 0;
+			}
+
+			if( this.RetreatStuckElapsed > (60 * 5) ) {
+				this.RetreatStuckElapsed = 0;
+
+				this.IsBraveNow = true;
+				this.IsRetreatingNow = false;
+			}
+		}
+	}
+}
