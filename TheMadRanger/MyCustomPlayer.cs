5d050f66bd42f37f75e447dcc159040ac90ad4f5
hamstar0 <webutek@gmail.com>
2021-08-03 11:34:20 -0700

    tweak

--- TheMadRanger/MyCustomPlayer.cs
+++ TheMadRanger/MyCustomPlayer.cs
@@ -11,14 +11,16 @@ using ModLibsNet.Services.Network;
 namespace TheMadRanger {
 	class TMRCustomPlayer : CustomPlayerData {
 		private static void MessageAboutReloadKey() {
-			Messages.MessagesAPI.AddMessage(
-				title: "Reminder: Bind your gun's reload key",
-				description: "Want more control over gun reloading? Be sure to bind it to a key. "
-					+"Also see the mod's configs for more options.",
-				modOfOrigin: TMRMod.Instance,
-				id: "TheMadRangerInfo",
-				parentMessage: Messages.MessagesAPI.ModInfoCategoryMsg
-			);
+			Messages.MessagesAPI.AddMessagesCategoriesInitializeEvent( () => {
+				Messages.MessagesAPI.AddMessage(
+					title: "Reminder: Bind your gun's reload key",
+					description: "Want more control over gun reloading? Be sure to bind it to a key. "
+						+ "Also see the mod's configs for more options.",
+					modOfOrigin: TMRMod.Instance,
+					id: "TheMadRangerInfo",
+					parentMessage: Messages.MessagesAPI.ModInfoCategoryMsg
+				);
+			} );
 		}
 
 
